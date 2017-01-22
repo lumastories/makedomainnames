15a051345cc1561cc0bd6c07d26cba9d6448f1cb
Nathan Nichols <nathann@ori.org>
2017-01-21 19:46:20 -0800

    sample data, fetches noun synonyms

--- Main.elm
+++ Main.elm
@@ -28,6 +28,7 @@ type alias Model =
     { word1 : Word
     , word2 : Word
     , word3 : Word
+    , error : String
     }
 
 
@@ -36,7 +37,7 @@ type alias Model =
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model {word="",synonyms=["1","2"]} {word="",synonyms=[]} {word="",synonyms=[]}
+    ( Model {word="",synonyms=["1","2"]} {word="",synonyms=[]} {word="",synonyms=[]} ""
     , Cmd.none )
 
 
@@ -47,6 +48,7 @@ view model =
         synonymItem s = 
             li [] [text s]
 
+        styles : List (String, String)
         styles = 
             [("margin", "20px")
             ,("font-family", "sans-serif")
@@ -55,9 +57,9 @@ view model =
         div [style styles]
             [ h1 [] [ text "make domain names" ]
             , input [ placeholder "enter a word", onInput UpdateWord1 ] []
-            
             , ul [] (List.map synonymItem model.word1.synonyms)
-
+            , button [onClick FetchSynonyms] [text "Load synonyms"]
+            , p [] [text model.error]
             --, input [ placeholder "enter a word", onInput UpdateWord2 ] []
             --, p [] [text (toString model.word2.synonyms)]
             --, input [ placeholder "enter a word", onInput UpdateWord3 ] []
@@ -66,7 +68,7 @@ view model =
 
 type Msg
     = FetchSynonyms
-    | NewSynonyms (Result Http.Error String)
+    | NewSynonyms (Result Http.Error (List String))
     | UpdateWord1 String
     | UpdateWord2 String
     | UpdateWord3 String
@@ -75,41 +77,40 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         UpdateWord1 s ->
-            ( model, Cmd.none )
+            ( { model | word1 = { word = s, synonyms = [] } }, Cmd.none )
+
+        FetchSynonyms ->
+            (model, getSynonyms model.word1.word)
+        
+        NewSynonyms (Ok newData) ->
+            ({model | word1 = {word = model.word1.word, synonyms = newData} }, Cmd.none)
+
+        NewSynonyms (Err s) ->
+            ( {model | error = (toString s) }, Cmd.none )
 
         _ ->
             ( model, Cmd.none )
 
 
-
---NewSynonyms (Ok newData) ->
---    ( { model | words = newData }, Cmd.none )
---NewSynonyms (Err _) ->
---    ( model, Cmd.none )
---UpdateWords newTopic ->
---    ( { model | word = newTopic }, Cmd.none )
---JD.list decodeWord |> JD.at [ "noun", "syn" ]
---JD.map2 Word
---    (JD.field "username" JD.string)
---    (JD.field "username" JD.string)
-
-
-getSynonyms : String -> Cmd Msg
+getSynonyms : String -> Cmd Msg 
 getSynonyms word =
     let
+        
         apikey = 
             ""
-
-        --TODO refactor this
+        
+        
         url =
             "http://words.bighugelabs.com/api/2/" ++ (apikey ++ ("/" ++ (word ++ "/json")))
 
+        -- Http.get takes a string and a decoder 
+        -- and returns a request object
         request =
             Http.get url decodeWordsUrl
     in
         Http.send NewSynonyms request
 
 
-decodeWordsUrl : JD.Decoder String
+decodeWordsUrl : JD.Decoder (List String)
 decodeWordsUrl =
-    JD.at [ "noun", "syn" ] JD.string
+    JD.at [ "noun", "syn" ] (JD.list JD.string)
